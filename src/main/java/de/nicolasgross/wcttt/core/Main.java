8a56c225ce94855ea7e22f1d942c4b83604e11eb
Nicolas Gross <nicolas@fam-gross.org>
2018-08-16 19:16:27 +0200

    Initial commit of core project

---
+++ src/main/java/de/nicolasgross/wcttt/core/Main.java
@@ -0,0 +1,11 @@
+package de.nicolasgross.wcttt.core;
+
+import de.nicolasgross.wcttt.lib.model.Semester;
+import de.nicolasgross.wcttt.lib.model.SemesterImpl;
+
+public class Main {
+
+	public static void main(String args[]) {
+		Semester semester = new SemesterImpl();
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module wcttt.core {
+	requires de.nicolasgross.wcttt.lib;
+
+	exports de.nicolasgross.wcttt.core;
+}
