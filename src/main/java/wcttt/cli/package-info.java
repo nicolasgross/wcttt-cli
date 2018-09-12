57132aecbccff25b5a81bafffa5dc77b12ce6adc
Nicolas Gross <nicolas@fam-gross.org>
2018-09-12 14:02:45 +0200

    Add package info

---
+++ src/main/java/wcttt/cli/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>Contains the implementation of the command line interface.</p>
+ *
+ * <p>Wcttt...Exception is thrown on errors that are relevant to or caused by the
+ * user.<br>
+ * Wcttt...FatalException is thrown on errors that are caused by a faulty
+ * implementation.</p>
+ */
+package wcttt.cli;
